d7481608bc9e107e00907a81bd752e4b0189e15c
Hannah Hudson <hannah.hch@gmail.com>
2017-08-16 12:32:40 -0400

    robots on page

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const mustache = require('mustache-express');
+const file = require("./data.js");
 
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.get('/:template', function(req, res) {
-  res.render('index', { name: 'example'});
+
+
+
+
+app.get('/users/', function(req, res) {
+  res.render('index', file);
+
 });
+
 
 
 app.listen(port, function(){
