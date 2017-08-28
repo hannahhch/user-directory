6904bd20c70a2733d0d9817a504c47a1f451e522
Hannah Hudson <hannah.hch@gmail.com>
2017-08-28 14:01:08 -0400

    directory site is the same but data is not accessed with mongodb instead of js file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const path = require('path');
 const mustache = require('mustache-express');
-const file = require("./data.js");
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoURL = 'mongodb://localhost:27017/newdb';
+
+
+
 
 app.engine('mustache', mustache());
 app.set('views', './views');
@@ -11,6 +15,14 @@ app.set('view engine', 'mustache');
 
 app.use(express.static(__dirname + '/public'));
 
+app.use('/', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
 
 app.get('/users/', function(req, res) {
   res.render('index', file);
@@ -21,7 +33,6 @@ app.get('/:id/', function(req, res) {
   res.render('bots', file.users[req.params.id-1]);
 });
 
-
-app.listen(port, function(){
-  console.log('Listening!')
+app.listen(3000,function(){
+  console.log("Listening for Mongo")
 });
